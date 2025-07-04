b2472dc4ed6947dca43608913409e5503dea3813
Viktor Dmitriyev <viktor@4vrs.com>
2025-07-04 14:45:17 +0500

    Core logic

--- redis.php
+++ redis.php
@@ -1,10 +1,10 @@
 <?php
 
 class Cache {
-  private static Redis $instance;
+  private static ?Redis $instance = null;
 
   public static function get_instance(): Redis {
-    if (!self::$instance instanceof Redis) {
+    if (self::$instance == null) {
       self::$instance = new Redis();
       self::$instance->connect('127.0.0.1', 6379);
     }
