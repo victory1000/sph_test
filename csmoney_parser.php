7e698002144fabd3d00ae41625510d0aa39b58be
Viktor Dmitriyev <viktor@4vrs.com>
2025-07-06 02:39:34 +0500

    Core logic

--- csmoney_parser.php
+++ csmoney_parser.php
@@ -2,8 +2,9 @@
 include_once "parser.php";
 
 foreach (Parser::getSkinsToParse() as $skin_name) {
-  $url = "https://cs.money/2.0/market/sell-orders?limit=60&offset=0&type=21&name=".rawurlencode($skin_name)."&order=asc&sort=price";
+  $url = "https://cs.money/2.0/market/sell-orders?limit=60&offset=0&type=21&name=die&order=asc&sort=price";
   $json = Parser::curl_exec($url);
+  error_log("\$json = ".print_r($json, true));
   $data = json_decode($json, true);
   error_log("\$data = ".print_r($data, true));
   break;
