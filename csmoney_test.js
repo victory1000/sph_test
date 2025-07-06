113b23e47fcb4c0f647bc6416d87cc54c76affc6
Viktor Dmitriyev <viktor@4vrs.com>
2025-07-06 17:27:48 +0500

    Core logic

--- csmoney_test.js
+++ csmoney_test.js
@@ -1,22 +1,3 @@
-// const puppeteer = require('puppeteer');
-//
-// (async () => {
-//   const browser = await puppeteer.launch({ headless: true });
-//   const page = await browser.newPage();
-//
-//   await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
-//
-//   await page.goto("https://cs.money/2.0/market/sell-orders?limit=60&offset=0&type=21&name=die&order=asc&sort=price", {
-//     waitUntil: 'networkidle2'
-//   });
-//
-//   const content = await page.content();
-//   console.log(content);
-//
-//   await browser.close();
-// })();
-
-
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -31,8 +12,9 @@ const puppeteer = require('puppeteer');
       waitUntil: 'networkidle0',
       timeout: 60000
     });
-    const content = await page.content();
-    console.log('✅ Заголовок страницы:', content);
+    // const content = await page.content();
+    const preText = await page.$eval('pre', el => el.innerText);
+    console.log('✅ Заголовок страницы:', preText);
     await browser.close();
   } catch (err) {
     console.error('❌ Ошибка при запуске Puppeteer:', err);
