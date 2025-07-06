b3d679040a96e86f82d203dc6a8613229f7c4e3a
Viktor Dmitriyev <viktor@4vrs.com>
2025-07-06 22:40:41 +0500

    Core logic

--- csmoney_test.js
+++ csmoney_test.js
@@ -10,10 +10,13 @@ puppeteer.use(StealthPlugin());
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
+    await page.setExtraHTTPHeaders({
+      'Accept-Language': 'en-US,en;q=0.9'
+    });
     await page.goto(url, {
       waitUntil: 'networkidle2',
       timeout: 60000
