aee100686d8aa8460b561e0ddb7d3f5c3413e1b2
Viktor Dmitriyev <viktor@4vrs.com>
2025-07-18 17:35:45 +0500

    Core logic

--- steam_parser_puppeteer.php
+++ steam_parser_puppeteer.php
@@ -24,7 +24,7 @@ class SteamParserPuppeteer extends SteamParser {
 
   protected function ParseSkins(): array {
     $listings = [];
-    $input = "";
+    $input = "test";
 
     $process = proc_open(
       'node /opt/sph_test/steam_ppt.js',
@@ -60,22 +60,22 @@ class SteamParserPuppeteer extends SteamParser {
     return $listings;
   }
 
-  protected function CheckSkins(array $listings): array {
+  protected function CheckSkins(array $to_check): array {
     error_log(__CLASS__.__METHOD__);
     $to_send = [];
-    if (empty($listings)) return $to_send;
+    if (empty($to_check)) return $to_send;
 
     foreach (Parser::getSkinsToParse() as $skin) {
-      foreach ($listings[$skin] as $listing_id => $data) {
+      foreach ($to_check[$skin] as $listing_id => $data) {
         if (in_array($listing_id, $this->sent)) {
-          unset($listings[$skin][$listing_id]);
+          unset($to_check[$skin][$listing_id]);
         }
       }
     }
 
     foreach ($this->getChats() as $chat_id => $skins) {
       foreach ($skins as $skin_name => $skin) {
-        foreach ($listings[$skin_name] ?? [] as $listing_id => $p_p) {
+        foreach ($to_check[$skin_name] ?? [] as $listing_id => $p_p) {
           $price_diff = round(($p_p['price'] * 100) / $this->price[$skin_name] - 100, 2);
           if (!$this->checkPatternPrice($skin_name, $p_p['pattern'], $price_diff)) continue;
           $to_send[$chat_id][] = [
