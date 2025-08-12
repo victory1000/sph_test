ebb3cc01ff30356204817778eb3a75f5b2c98822
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-12 18:19:09 +0500

    Core logic

--- sph_bot.php
+++ sph_bot.php
@@ -4,16 +4,22 @@ include_once "parser.php";
 $start_mem = memory_get_usage();
 $start_time = microtime(true);
 
-try {
-  $_steam = new SteamParserPuppeteer();
-  $_steam->Process();
-  $_steam->Debug("Execution", [
-    'time' => microtime(true) - $start_time,
-    'memory' => round((memory_get_usage() - $start_mem) / 1048576.00, 2),
-    'max memory' => memory_get_peak_usage() / 1048576.00
-  ]);
-} catch (Throwable $e) {
-  $message = "Exception: " . $e->getMessage() . " in file " . $e->getFile() . " on line " . $e->getLine() . PHP_EOL;
-  $message .= "Backtrace:" . PHP_EOL . $e->getTraceAsString();
-  Parser::ErrorTG($message);
+$time = 30;
+while ($time < 60) {
+  try {
+    $_steam = new SteamParserPuppeteer();
+    $_steam->Process();
+    $processing_time = microtime(true) - $start_time;
+    $time += max($processing_time, 25);
+    error_log("time = $time");
+    $_steam->Debug("Execution", [
+      'time' => $processing_time,
+      'memory' => round((memory_get_usage() - $start_mem) / 1048576.00, 2),
+      'max memory' => memory_get_peak_usage() / 1048576.00
+    ]);
+  } catch (Throwable $e) {
+    $message = "Exception: " . $e->getMessage() . " in file " . $e->getFile() . " on line " . $e->getLine() . PHP_EOL;
+    $message .= "Backtrace:" . PHP_EOL . $e->getTraceAsString();
+    Parser::ErrorTG($message);
+  }
 }
