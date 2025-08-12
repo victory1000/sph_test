6091ef77b0487f560c6cf8c18714115a7187c409
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-13 02:58:40 +0500

    Core logic

--- sph_bot.php
+++ sph_bot.php
@@ -8,27 +8,27 @@ if (!flock($lockFile, LOCK_EX | LOCK_NB)) {
   fclose($lockFile);
   exit;
 }
-//error_log('start');
+error_log('start');
 
 $time = 30;
-//while ($time < 60) {
+while ($time < 60) {
   $start_mem = memory_get_usage();
   $start_time = microtime(true);
   try {
     $_steam = new SteamParserPuppeteer();
     $_steam->Process();
     $processing_time = microtime(true) - $start_time;
-//    error_log("\$processing_time = ".$processing_time);
+    error_log("\$processing_time = ".$processing_time);
     $time += max($processing_time, 25);
-//    error_log("time = $time");
+    error_log("time = $time");
     $_steam->Debug("Execution", [
       'time' => $processing_time,
       'memory' => round((memory_get_usage() - $start_mem) / 1048576.00, 2),
       'max memory' => memory_get_peak_usage() / 1048576.00
     ]);
     if ($time < 60) {
-//      error_log("sleep = ".(30-$processing_time));
-//      sleep(30-$processing_time);
+      error_log("sleep = ".(30-$processing_time));
+      sleep(30-$processing_time);
     }
   } catch (Throwable $e) {
     $message = "Exception: " . $e->getMessage() . " in file " . $e->getFile() . " on line " . $e->getLine() . PHP_EOL;
@@ -37,8 +37,8 @@ $time = 30;
     fclose($lockFile);
     exit;
   }
-//}
-//error_log('end');
+}
+error_log('end');
 
 flock($lockFile, LOCK_UN);
 fclose($lockFile);
