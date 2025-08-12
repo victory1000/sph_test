e958c0c28d1685a8669a6eeabc372cc725c19aa4
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-12 10:25:14 +0500

    Core logic

--- steam_ppt.js
+++ steam_ppt.js
@@ -70,6 +70,11 @@ process.stdin.on('data', async chunk => {
 
             if (page.isClosed()) {
               page = await browser.newPage();
+              await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
+              await page.setExtraHTTPHeaders({
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Origin': "chrome-extension://jjicbefpemnphinccgikpdaagjebbnhg"
+              });
             }
 
             await page.goto("https://api.csfloat.com/?url=" + _data.inspect, {
