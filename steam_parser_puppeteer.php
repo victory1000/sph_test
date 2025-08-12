42b0403c2448c42a9d71e644d6ff56bd8029e237
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-12 16:48:56 +0500

    Core logic

--- steam_parser_puppeteer.php
+++ steam_parser_puppeteer.php
@@ -25,7 +25,6 @@ class SteamParserPuppeteer extends SteamParser {
   protected function ParseSkins(): array {
     $redis_processed = json_decode($this->_redis->get('processed_listings'), true, flags: JSON_BIGINT_AS_STRING) ?? [];
     array_walk($redis_processed, fn(&$el) => $el = (string)$el);
-    $redis_processed = []; // TODO Delete
     $input = json_encode($redis_processed);
 
     $this->Debug("processed_listings", $redis_processed);
