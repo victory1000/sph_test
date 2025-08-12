d2b899b3a51664a48373aa63a06ab56388b5e24f
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-12 17:39:13 +0500

    Core logic

--- steam_parser.php
+++ steam_parser.php
@@ -139,14 +139,15 @@ class SteamParser {
       513209606 => [
         "Charm | Baby's AK" => [
           ['pattern_m' => 99_000, 'pattern_l' => 100_000, 'price_percent' => 30],
-          ['pattern_m' => 1, 'pattern_l' => 1000, 'price_percent' => 30],
+//          ['pattern_m' => 1, 'pattern_l' => 1000, 'price_percent' => 30],
+          ['pattern_m' => 20000, 'pattern_l' => 50000, 'price_percent' => 30],
         ],
         "Charm | Die-cast AK" => [
           ['pattern_m' => 87_000, 'pattern_l' => 100_000, 'price_percent' => 30],
           ['pattern_m' => 1, 'pattern_l' => 24_000, 'price_percent' => 30],
         ],
         "Charm | Titeenium AWP" => [
-//          ['pattern_m' => 99_000, 'pattern_l' => 1000, 'price_percent' => 30],
+          ['pattern_m' => 99_000, 'pattern_l' => 100_000, 'price_percent' => 30],
           ['pattern_m' => 1, 'pattern_l' => 13_000, 'price_percent' => 30],
         ],
         "Charm | Disco MAC" => [
@@ -154,7 +155,7 @@ class SteamParser {
           ['pattern_m' => 1, 'pattern_l' => 5_000, 'price_percent' => 30],
         ],
         "Charm | Glamour Shot" => [
-//          ['pattern_m' => 89_000, 'pattern_l' => 100_000, 'price_percent' => 30],
+          ['pattern_m' => 99_000, 'pattern_l' => 100_000, 'price_percent' => 30],
           ['pattern_m' => 1, 'pattern_l' => 4000, 'price_percent' => 30],
         ],
       ]
