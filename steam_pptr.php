8c96eed57dabae5fa3526b6aa4f8a14d60500308
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-12 11:30:28 +0500

    Core logic

--- steam_pptr.php
+++ steam_pptr.php
@@ -1,5 +1,13 @@
 <?php
 include_once "parser.php";
 
+$start_mem = memory_get_usage();
+$start_time = microtime(true);
+
 $_steam = new SteamParserPuppeteer();
 $_steam->Process();
+
+error_log("Execution: ".print_r([
+    'time' => microtime(true) - $start_time,
+    'memory' => round((memory_get_usage() - $start_mem) / 1048576.00, 2),
+    'max memory' => memory_get_peak_usage() / 1048576.00], true)."\n");
