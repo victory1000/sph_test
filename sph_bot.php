b084de5163e5be830cd40cb7b3bc13c124f6946b
Viktor Dmitriyev <viktor@4vrs.com>
2025-08-12 18:44:46 +0500

    Core logic

--- sph_bot.php
+++ sph_bot.php
@@ -1,6 +1,6 @@
 <?php
 include_once "parser.php";
-error_log('start');
+//error_log('start');
 
 $time = 30;
 //while ($time < 60) {
@@ -10,16 +10,16 @@ $time = 30;
     $_steam = new SteamParserPuppeteer();
     $_steam->Process();
     $processing_time = microtime(true) - $start_time;
-    error_log("\$processing_time = ".$processing_time);
+//    error_log("\$processing_time = ".$processing_time);
     $time += max($processing_time, 25);
-    error_log("time = $time");
+//    error_log("time = $time");
     $_steam->Debug("Execution", [
       'time' => $processing_time,
       'memory' => round((memory_get_usage() - $start_mem) / 1048576.00, 2),
       'max memory' => memory_get_peak_usage() / 1048576.00
     ]);
     if ($time < 60) {
-      error_log("sleep = ".(30-$processing_time));
+//      error_log("sleep = ".(30-$processing_time));
 //      sleep(30-$processing_time);
     }
   } catch (Throwable $e) {
@@ -28,7 +28,7 @@ $time = 30;
     Parser::ErrorTG($message);
   }
 //}
-error_log('end');
+//error_log('end');
 
 //$lockFile = fopen(__DIR__ . '/script.lock', 'c');
 //
